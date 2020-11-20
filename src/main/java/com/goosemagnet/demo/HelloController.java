d21fc41b6c93f193143560a529913bde2e493798
GooseMagnet <ilankarasik@gmail.com>
2020-11-19 19:39:26 -0500

    Added endpoint for name

--- src/main/java/com/goosemagnet/demo/HelloController.java
+++ src/main/java/com/goosemagnet/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.goosemagnet.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class HelloController {
     @RequestMapping("/time")
     public String getTime() {
         return new Date().toString();
+    }
+
+    @RequestMapping(path="/{name}")
+    public String helloWorld(@PathVariable("name") String name) {
+        return String.format("Hello, %s!", name);
     }
 }
